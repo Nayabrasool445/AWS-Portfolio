8fd4a4eaa641a39f24c6dfaa412fe357d96e75ac
Shaik Nayab Rasool <shaik.nayabrasool@greenwayhealth.com>
2025-05-03 00:01:52 +0530

    Add Project 1 - VPC + EC2 Terraform setup

---
+++ outputs.tf
@@ -0,0 +1,24 @@
+output "vpc_id" {
+  description = "The ID of the VPC"
+  value       = aws_vpc.migration_vpc.id
+}
+
+output "ec2_public_ip" {
+  description = "The public IP address of the EC2 instance"
+  value       = aws_instance.Public_ec2.public_ip
+}
+
+output "ec2_name" {
+  description = "The name of the EC2 instance"
+  value       = aws_instance.Public_ec2.tags["Name"]
+}
+
+output "security_group_id" {
+  description = "The ID of the Security Group"
+  value       = aws_security_group.Public_ec2_sg.id
+}
+
+output "security_group_name" {
+  description = "The name of the Security Group"
+  value       = aws_security_group.Public_ec2_sg.name
+}
